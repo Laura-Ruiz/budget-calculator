24c7da7e2981542867f733ea20a5a050ed83d793
Laura-Ruiz <laaura.26@gmail.com>
2022-07-05 19:23:57 +0200

    Added increment and decrement button [Exercise 3]

--- src/components/Panell.js
+++ src/components/Panell.js
@@ -1,29 +1,78 @@
 import React from 'react'
 
 
-export default function Panell(props){
-   
-        return(
-        <div>
-        <div>
-        <label>Número de páginas</label>
-        <input
-        type="number"
-        name="numPag"
-        id="numPag"
-        value={props.formData.numPag}
-        onChange={props.handleChange}
-        />
-        </div>
-        <div>
-        <label>Número de idiomas</label>
-        <input
-        type="number"
-        name="numIdiom"
-        id="numIdiom"
-        value={props.formData.numIdiom}
-        onChange={props.handleChange}
-        />
-       </div>
-    </div>
-)}
+export default function Panell(props) {
+
+   function incrementar(event) {
+      let { name } = event.target
+      props.setFormData(prevFormData => {
+         return {
+            ...prevFormData,
+            [name]: name === "numPag" ? props.formData.numPag++ : props.formData.numIdiom++
+         }
+      })
+
+   }
+
+   function disminuir(event) {
+      let { name } = event.target
+      props.setFormData(prevFormData => {
+         // if (props.formData.numPag = 0 || props.formData.numIdiom === 0) {
+         //    return 0
+         // }
+         return {
+            ...prevFormData,
+            [name]: name === "numPag" ? props.formData.numPag-- : props.formData.numIdiom--
+         }
+      })
+
+   }
+
+   return (
+      <div>
+         <label>Número de páginas</label>
+         <input
+            type="button"
+            value="+"
+            onClick={incrementar}
+            name="numPag"
+         />
+         <input
+            type="number"
+            name="numPag"
+            id="numPag"
+            value={props.formData.numPag}
+            onChange={props.handleChange}
+
+         />
+         <input
+            type="button"
+            value="-"
+            onClick={disminuir}
+            name="numPag"
+         />
+         <div>
+            <label>Número de idiomas</label>
+            <input
+               type="button"
+               value="+"
+               onClick={incrementar}
+               name="numIdiom"
+            />
+            <input
+               type="number"
+               name="numIdiom"
+               id="numIdiom"
+               value={props.formData.numIdiom}
+               onChange={props.handleChange}
+            />
+            <input
+               type="button"
+               value="-"
+               onClick={disminuir}
+               name="numIdiom"
+            />
+         </div>
+      </div>
+   )
+}
